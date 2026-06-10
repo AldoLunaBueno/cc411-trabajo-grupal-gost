bcf9e9c73c6000a7d2869229d0edc42d233c0d68
AriusJoel1 <joel.seminario.s@uni.pe>
2026-06-09 21:57:29 -0500

    Implement formatToGostInteger and verify RFC vectors

--- gost/src/main/java/uni/modules/hash/Streebog.java
+++ gost/src/main/java/uni/modules/hash/Streebog.java
@@ -2,14 +2,16 @@ package uni.modules.hash;
 
 import java.math.BigInteger;
 
+import org.bouncycastle.crypto.Digest;
+import org.bouncycastle.crypto.digests.GOST3411_2012_256Digest;
+import org.bouncycastle.crypto.digests.GOST3411_2012_512Digest;
+
 import uni.utils.GostUtils;
 
-// Benja
 public class Streebog implements HashService {
 
     private final int bitLength;
 
-    // Constructor que define el comportamiento de esta instancia
     public Streebog(int bitLength) {
         if (bitLength != 256 && bitLength != 512) {
             throw new IllegalArgumentException("El estándar Streebog solo soporta 256 o 512 bits");
@@ -19,19 +21,36 @@ public class Streebog implements HashService {
 
     @Override
     public BigInteger computeHashInteger(byte[] message, BigInteger q) {
-        // Usa la variable de instancia en lugar del número estático
         byte[] rawHashBytes = computeRawHash(message, this.bitLength);
         return formatToGostInteger(rawHashBytes, q);
     }
 
     public byte[] computeRawHash(byte[] message, int bitLength) {
-        throw new UnsupportedOperationException("Unimplemented method 'computeRawHashHex'");
+        Digest digest;
+
+        if (bitLength == 256) {
+            digest = new GOST3411_2012_256Digest();
+        } else {
+            digest = new GOST3411_2012_512Digest();
+        }
+
+        digest.update(message, 0, message.length);
+
+       byte[] output = new byte[digest.getDigestSize()];
+        digest.doFinal(output, 0);
+
+        return GostUtils.reverseBytes(output);
     }
 
     public BigInteger formatToGostInteger(byte[] rawHashBytes, BigInteger q) {
-        // a) Convertir usando utilidades a BigEndian
-        // b) Aplicar módulo q
-        // c) Regla rusa (e=0 -> e=1)
-        throw new UnsupportedOperationException("Unimplemented method 'formatToGostInteger'");
+        BigInteger alpha = GostUtils.fromLittleEndianBytes(rawHashBytes);
+
+        BigInteger e = alpha.mod(q);
+
+        if (e.equals(BigInteger.ZERO)) {
+            e = BigInteger.ONE;
+        }
+
+        return e;
     }
 }
